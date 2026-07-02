a6b164ac7398e9e69a51abe175219eebef4aa812
League Student <student@jointheleague.org>
2026-07-02 12:47:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
@@ -13,7 +13,10 @@ Refer to the previous program, Meet_Tina.py, for examples of how to use turtle c
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
-
+tina.forward(30)
+tina.left(72)
+tina.forward(30)
+tina.left()
 # Use tina.forward() and tina.left() to draw a pentagon
 # Make each side of the pentagon a different color with 
 # tina.pencolor()
