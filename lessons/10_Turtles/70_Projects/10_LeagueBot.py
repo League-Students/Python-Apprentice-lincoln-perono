8bf52af184697276433e4f700482277cefb9a14b
League Student <student@jointheleague.org>
2026-07-07 13:11:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -14,8 +14,8 @@ screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
-t = turtle.Turtle()
-import turtle
+
+
 
 def set_turtle_image(turtle, image_name):
     """Set the turtle's shape to a custom image."""
