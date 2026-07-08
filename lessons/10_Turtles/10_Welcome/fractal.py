12f530e4c957c3d6d8aa3c81bec91240231df73b
League Student <student@jointheleague.org>
2026-07-08 12:45:01 -0700

    Auto-commit: workspace changes before push

---
+++ lessons/10_Turtles/10_Welcome/fractal.py
@@ -0,0 +1,18 @@
+import turtle
+
+tina = turtle.turtle()
+screen = turtle.screen()
+sccreen.setup(600,600)
+
+def fractal_triangle(size,depth):
+    if depth == 0:
+        for i in range(3):
+            tina.forward(size)
+            tina.left(120)
+    else:
+        for i in range(3)
+            fractal_triangle(size/2,depth-1)
+            
+fractal_triangle(200,1)
+
+turtle.excitonclick()
