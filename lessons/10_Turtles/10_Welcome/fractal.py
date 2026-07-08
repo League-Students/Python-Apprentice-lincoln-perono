d4318c4e3615be3f5ddbd9e26e374c714f676e53
League Student <student@jointheleague.org>
2026-07-08 12:49:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/fractal.py
+++ lessons/10_Turtles/10_Welcome/fractal.py
@@ -12,7 +12,7 @@ def fractal_triangle(size,depth):
     else:
         for i in range(3):
             fractal_triangle(size/2,depth-1)
-            tina.forward
+            tina.forward(sizes)
             
 fractal_triangle(200,1)
 
