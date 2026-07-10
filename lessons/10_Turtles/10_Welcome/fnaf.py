9b88b36901eef3682658fb43252429de33f15a2c
League Student <student@jointheleague.org>
2026-07-10 13:00:31 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/fnaf.py
+++ lessons/10_Turtles/10_Welcome/fnaf.py
@@ -10,7 +10,8 @@ screen.setup(500,500)
 
 cam_colors = ["red","black","white","blue","green"]
 
-def show_am
+def show_animatronics(cam_num):
+    if
 def open_cam1():
     print("cam_1 open")
     screen.bgcolor(cam_colors[0])
