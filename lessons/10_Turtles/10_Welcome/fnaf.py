3ee8ea6d3956c85379e72d5881e915d4a823e201
League Student <student@jointheleague.org>
2026-07-10 12:39:14 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/fnaf.py
+++ lessons/10_Turtles/10_Welcome/fnaf.py
@@ -5,5 +5,5 @@ tina = turtle.Turtle()
 screen=turtle.Screen()
 screen.setup(500,500)
 
-cam_colors = ["red","black","white",""]
+cam_colors = ["red","black","white","blue","green"]
 turtle.exitonclick()
